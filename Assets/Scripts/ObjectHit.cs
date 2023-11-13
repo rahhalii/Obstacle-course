77500dc1eff843c57ae8b564d430a0819eb2704b
rahhalii <rahatali152@gmail.com>
2023-11-13 15:22:25 +0530

    commit

---
+++ Assets/Scripts/ObjectHit.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+   private void OnCollisionEnter(Collision other)
+   {
+       if(other.gameObject.tag == "Player")
+       {
+        GetComponent<MeshRenderer>().material.color = Color.red;
+        gameObject.tag = "Hit";
+       }
+       
+   }
+}
